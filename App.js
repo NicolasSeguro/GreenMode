479d26344b028f2403ff7714bc7f2cce27a0e0eb
NicolasSeguro <nicolas.seguro@gmail.com>
2021-08-30 23:49:16 -0300

    Agrego fonts

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { isLoaded, useFonts } from 'expo-font';
 
+import AppLoading from 'expo-app-loading';
 import MainNavigation from './src/navigation/MainNavigation';
 import { Provider as PaperProvider } from 'react-native-paper'
 import React from 'react';
 import auth from './src/screens/Auth';
 
 export default function App() {
+
+  consta [loaded, error] = useFonts({
+    'nunito': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+   if (!loaded) return <AppLoading/>
   return (
     <PaperProvider>
       <MainNavigation></MainNavigation>
