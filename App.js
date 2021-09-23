0cdc94aa1160ee9d922d0c76b071022fef370ad2
NicolasSeguro <nicolas.seguro@gmail.com>
2021-09-22 22:06:15 -0300

    Form de registro y login

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import MainNavigation from './src/navigation/MainNavigation';
 import { Provider as PaperProvider } from 'react-native-paper'
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import React from 'react';
 import auth from './src/screens/Auth';
-import store from './store'
+// import store from './store'
 import { useFonts } from 'expo-font';
 
 export default function App() {
--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,16 +1,19 @@
-import { ScrollView, Text } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, ScrollView, Text } from 'react-native'
+import React, { useState } from 'react'
 
-import React from 'react'
+import RegisterForm from '../components/Auth/RegisterForm'
 import SearchBar from '../components/Search/SearchBar'
 import { Searchbar } from 'react-native-paper'
 
 export default function auth() {
+    const [showLogin, setShowLogin] = useState(false);
     return (
         <>
-        <Searchbar/>
         <ScrollView>
-            
+            <KeyboardAvoidingView>
+                {showLogin ? <Text>Inicia sesion </Text> : <RegisterForm />}
+            </KeyboardAvoidingView>
         </ScrollView>
         </>
-    )
+    );
 }
